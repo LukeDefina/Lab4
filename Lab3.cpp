56547b4c4b758d3325211dce003c12f3c43d3744
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2022-12-26 12:28:40 -0500

    Update Lab3.cpp

--- Lab3.cpp
+++ Lab3.cpp
@@ -1,11 +1,8 @@
-// Workers at a particular company have won a 7.6% pay increase 
-// retroactive for six months.
-// Write a program that prompts a user for their full name 
-// and previous annual salary as inputs and then calculate 
-// and output a report of the amount of retroactive pay due 
-// the employee, the new annual salary, and the new monthly 
-// salary using the given format.
-// Complete the gien code for the main function
+// Write a program that prompts the user to enter two 
+// primary colors as strings: “red”, “green”, or “blue”. 
+// Once entered, the program outputs the resulting secondary color. 
+// The program should be designed to respond to invalid colors or 
+// duplicate colors such as blue mixed with blue.
 
 #include <iostream>
 
