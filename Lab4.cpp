acd6707080a56cb644f13500be841986c96db3c9
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2023-01-02 19:11:10 -0500

    Create Lab4.cpp

---
+++ Lab4.cpp
@@ -0,0 +1,12 @@
+// Write a program that prompts the user to enter two 
+// primary colors as strings: “red”, “green”, or “blue”. 
+// Once entered, the program outputs the resulting secondary color. 
+// The program should be designed to respond to invalid colors or 
+// duplicate colors such as blue mixed with blue.
+
+#include <iostream>
+
+int main() {
+    
+    return 0;
+}
